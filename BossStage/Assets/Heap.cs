6ca546057e706b1b6ec0503e123b371aa25025c1
Mattia Linguerri <m.linguerri@yahoo.it>
2018-06-08 12:52:14 +0200

    first commit

---
+++ BossStage/Assets/Heap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heap<T> {
+
+    T[] items;
+    int currentItemCount;
+
+    public Heap(int maxSize)
+    {
+        items = new T[maxSize];
+    }
+
+    public void Add(T item)
+    {
+
+    }
+}
